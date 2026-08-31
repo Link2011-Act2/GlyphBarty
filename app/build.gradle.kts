2945d5de39147c8c6ce0d4ad68ca82c82cd5da49
Azul1nk <www.rikus0205.co.jp@gmail.com>
2026-09-01 02:10:08 +0900

    贅沢な名だねぇ、お前は所詮devビルドだ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ val buildNumberFiles = (
 ).sortedBy { it.relativeTo(rootProject.projectDir).invariantSeparatorsPath }
 
 val appCodeName = "Fricle" // 0.x.0のxが変わるたびに更新したいね トリッカルの使徒から取る
-val appVersionName = "2.0.2-IntDev_rev0"
+val appVersionName = "2.0.2-IntDev_rev1"
 val buildContentHash = MessageDigest.getInstance("SHA-256").run {
     buildNumberFiles.forEach { file ->
         update(file.relativeTo(rootProject.projectDir).invariantSeparatorsPath.toByteArray())
@@ -57,7 +57,7 @@ android {
         applicationId = "jp.linkserver.glyphvisualizer"
         minSdk = 33
         targetSdk = 36
-        versionCode = 12
+        versionCode = 13
         versionName = appVersionName
         buildConfigField("String", "BUILD_NUMBER", "\"$generatedBuildNumber\"")
 
