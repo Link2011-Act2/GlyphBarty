68882b81decaa6e95e0cef8f8fa9f55917bd0081
Azul1nk <www.rikus0205.co.jp@gmail.com>
2026-09-03 00:13:32 +0900

    世に射精すためにはこれが必要やね

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ val buildNumberFiles = (
 ).sortedBy { it.relativeTo(rootProject.projectDir).invariantSeparatorsPath }
 
 val appCodeName = "Picora" // 0.x.0のxが変わるたびに更新したいね トリッカルの使徒から取る
-val appVersionName = "2.1.0-IntDev_rev2"
+val appVersionName = "2.1.0-Release"
 val buildContentHash = MessageDigest.getInstance("SHA-256").run {
     buildNumberFiles.forEach { file ->
         update(file.relativeTo(rootProject.projectDir).invariantSeparatorsPath.toByteArray())
